66bb1705cf929c418a3caa423c219df7bc9dd4a1
Drakzel <kris501i@easv365.dk>
2018-08-26 20:05:26 +0200

    Init commit
    Brackeys - movement (ep1).

---
+++ Brackeys RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Left click to move player
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //Move player to hit
+                motor.MoveToPoint(hit.point);
+
+                //Stop focusing any objects
+            }
+        }
+
+        //Right click to interact with object
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //Check if we hit interactable
+                //If we did, set it to our focus
+            }
+        }
+    }
+}
